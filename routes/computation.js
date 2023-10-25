f8b3f703ea40df74cfc853687a390dcd56ce2acc
AbhilashGadiparthi <143052901+AbhilashGadiparthi@users.noreply.github.com>
2023-10-25 17:55:15 -0500

    Update computation.js

--- routes/computation.js
+++ routes/computation.js
@@ -1,32 +1,47 @@
 var express = require('express');
+ 
 var router = express.Router();
-var rand1 = Math.random() * 10;
-var rand2 = Math.random() * 20;
-
-var num1 = 0;
-var num2 = 0;
-var num3 = 0;
-
  
-
+var random1 = Math.random() * 10;
+ 
+var random2 = Math.random() * 20;
+ 
+ 
+var number1 = 0;
+ 
+var number2 = 0;
+ 
+var number3 = 0;
+ 
+ 
+ 
+ 
 router.get('/', function (req, res, next) {
-
-  num1 = Math.floor(rand1);
-  num2 = Math.floor(rand2);
-  num3 = Math.floor(rand1);
-
  
-
-  res.send(`The Random numbers are ${rand1} and ${rand2}
-
-    <br> <br> floor applied to ${rand1} is ${num1}
-
-    <br> <br> floor applied to ${rand2} is ${num2}
-
-    <br> <br> floor applied to ${rand1} is ${num3}`);
-
+ 
+  number1 = Math.log2(random1);
+ 
+  number2 = Math.cosh(random2);
+ 
+  number3 = Math.floor(random1);
+ 
+ 
+ 
+ 
+  res.send(`The Random numbers are ${random1} and ${random2}
+ 
+ 
+    <br> <br> log2 applied to ${random1} is ${number1}
+ 
+ 
+    <br> <br> cosh applied to ${random2} is ${number2}
+ 
+ 
+    <br> <br> floor applied to ${random1} is ${number3}`);
+ 
+ 
 });
-
  
 
+ 
 module.exports = router;
